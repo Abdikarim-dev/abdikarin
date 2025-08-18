a2f62815f34797854a8df92789a0644db3273fb7
Abdikarim-dev <cabdikariim405@gmail.com>
2025-08-18 20:09:06 +0300

    fixed some issue

--- components/recent-works.tsx
+++ components/recent-works.tsx
@@ -513,14 +513,14 @@ const PosterCard = ({
   return (
     <Card
       className="bg-white/5 border-white/10 overflow-hidden group relative transition-all duration-300 hover:bg-white/10 hover:border-white/20"
-      style={{
-        height:
-          typeof window !== "undefined" && window.innerWidth < 640
-            ? "240px"
-            : window.innerWidth < 1024
-              ? "300px"
-              : "360px",
-      }}
+              style={{
+          height:
+            typeof window !== "undefined" && window.innerWidth < 640
+              ? "240px"
+              : typeof window !== "undefined" && window.innerWidth < 1024
+                ? "300px"
+                : "360px",
+        }}
     >
       {/* Loading State */}
       {!imageLoaded && !imageError && (
